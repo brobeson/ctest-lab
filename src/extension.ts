a3ef754b0bc066a8fa41c1308ddf61b1e8de82f2
brobeson <brobeson@users.noreply.github.com>
2022-08-14 13:08:03 -0600

    Create and populate a test controller
    I create and populate a test controller. The test items are from
    hard-coded `ctest --show-only` output. I also changed the
    extension activation to workspaces containing CMakeLists.txt
    files.

--- src/extension.ts
+++ src/extension.ts
@@ -16,10 +16,37 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('ctest-lab.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ctest-lab!');
+		const config = vscode.workspace.getConfiguration("cmake");
+		if (config.has("buildDirectory")) {
+			vscode.window.showInformationMessage(`Reading tests from ${config.get("buildDirectory")}`);
+		} else {
+			vscode.window.showErrorMessage("No cmake.buildDirectory defined.");
+		}
 	});
 
 	context.subscriptions.push(disposable);
+
+	const controller = vscode.tests.createTestController("ctest-lab-tests", "CTest");
+	context.subscriptions.push(controller);
+
+	const ctest_output = `Test project /home/brendan/repositories/tracking-analyzer/build
+	Test #1: bounding_box_test
+	Test #2: dataset_test
+	Test #3: exceptions_test
+	Test #4: filesystem_test
+	Test #5: results_database_test
+	Test #6: sequence_results_test
+	Test #7: tracker_results_test
+	Test #8: training_metadata_test
+	`;
+	const lines = ctest_output.trim().split('\n');
+	for (const line of lines) {
+		if (line.trim().startsWith("Test #")) {
+			const i = line.indexOf(":")
+			const case_name = line.substring(i + 1).trim()
+			controller.items.add(controller.createTestItem(case_name, case_name))
+		}
+	}
 }
 
 // this method is called when your extension is deactivated
