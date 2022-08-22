f0134021af5d78e121211d338d1be9417e57b9de
brobeson <brobeson@users.noreply.github.com>
2022-08-22 15:00:59 -0600

    Fix ctest output parsing
    I changed the parse code to use a regex instead of `startswith()`.
    CTest right-aligns the test numbers, so with more than 9 tests,
    there are multiple spaces between "Test" and the "#".

--- src/ctest_lab.ts
+++ src/ctest_lab.ts
@@ -6,12 +6,13 @@ export async function discover_tests(test_controller: vscode.TestController, log
   const ctest_output = await run_ctest_show_only(log_channel);
   const lines = ctest_output.trim().split('\n');
   for (const line of lines) {
-    if (line.trim().startsWith("Test #")) {
+    if (line.match(/\s+Test\s+#/)) {
       const i = line.indexOf(":");
       const case_name = line.substring(i + 1).trim();
       test_controller.items.add(test_controller.createTestItem(case_name, case_name));
     }
   }
+  log_channel.appendLine(`Found ${test_controller.items.size} tests.`);
 }
 
 function run_ctest_show_only(log_channel: vscode.OutputChannel): Promise<string> {
@@ -19,16 +20,22 @@ function run_ctest_show_only(log_channel: vscode.OutputChannel): Promise<string>
   return new Promise((resolve, reject) => {
     const process = spawn("ctest", ["--show-only"], { cwd: get_build_directory() });
     let stdout = "";
+    let stderr = "";
     if (process.pid) {
       process.stdout.on("data", data => {
         stdout += data;
-        log_channel.append(data);
       });
       process.stdout.on("end", () => {
+        log_channel.appendLine(stdout);
         resolve(stdout);
       });
       process.stderr.on("data", data => {
-        log_channel.append(data);
+        stderr += data;
+      });
+      process.stderr.on("end", () => {
+        if (stderr.length > 0) {
+          log_channel.appendLine(stderr);
+        }
       });
       process.on("error", err => {
         log_channel.appendLine(err.message);
